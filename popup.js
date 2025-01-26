261b42ba826143ab68e7d9175e4524c2f1cd48d6
조승우 <choseung97@gmail.com>
2025-01-26 15:00:54 +0900

    feat(popup): Auto title toggle 기능 추가
    - html toggle 추가
    - toggle On/OFF EventListner 추가

--- popup.js
+++ popup.js
@@ -1,19 +1,42 @@
 /* global chrome */
-document.addEventListener('DOMContentLoaded', async () => {
-  const toggleSwitch = document.getElementById('extension-toggle');
+document.addEventListener('DOMContentLoaded', () => {
+  const extensionToggle = document.getElementById('extension-toggle');
+  const autoTitleToggle = document.getElementById('auto-title-toggle');
 
-  const { enabled = true } = await chrome.storage.local.get('enabled');
-  toggleSwitch.checked = enabled;
+  chrome.storage.local.get(['enabled', 'autoTitleEnabled'], (result) => {
+    const enabled = result.enabled !== undefined ? result.enabled : true;
+    const autoTitleEnabled
+      = result.autoTitleEnabled !== undefined ? result.autoTitleEnabled : true;
+    extensionToggle.checked = enabled;
+    autoTitleToggle.checked = autoTitleEnabled;
+  });
 
-  toggleSwitch.addEventListener('change', async () => {
-    const isEnabled = toggleSwitch.checked;
-
-    await chrome.storage.local.set({ enabled: isEnabled });
-
+  extensionToggle.addEventListener('change', (e) => {
+    const enabled = e.target.checked;
+    const autoTitleEnabled = autoTitleToggle.checked;
+    chrome.storage.local.set({ enabled });
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.tabs.sendMessage(tabs[0].id, {
         type: 'TOGGLE_EXTENSION',
-        enabled: isEnabled,
+        enabled,
+        autoTitleEnabled,
+      });
+    });
+  });
+
+  autoTitleToggle.addEventListener('change', (e) => {
+    const autoTitleEnabled = e.target.checked;
+    const enabled = extensionToggle.checked;
+    chrome.storage.local.set({ autoTitleEnabled });
+    if (!enabled) {
+      autoTitleToggle.checked = false;
+      return;
+    }
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        type: 'TOGGLE_AUTO_TITLE',
+        enabled,
+        autoTitleEnabled,
       });
     });
   });
